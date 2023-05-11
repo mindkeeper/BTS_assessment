592450e8106ca694cdf2b26498cf8c3e29703f78
mindkeeper <fcb.nyak@gmail.com>
2023-05-11 16:52:53 +0700

    deleting checklist item routes

--- src/routes/checklistItem.js
+++
@@ -1,7 +0,0 @@
-const createChecklistHandler = require("../controllers/checklist/createChecklist");
-const isLogin = require("../middlewares/isLogin");
-
-const Router = require("express").Router();
-
-Router.post("/:id/item", isLogin, createChecklistHandler);
-module.exports = Router;
